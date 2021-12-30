0b8af7a12c4540f58681d721438988509e6c51a2
AlxndrHernandez <alxndr.hernandez@gmail.com>
2021-12-30 10:57:41 -0300

    se crea json y servidor basico gin

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
 
+// Crea router con gin
 func main() {
 
-	fmt.Println("hola")
+	router := gin.Default()
+
+	// captura la solicitud get "/hola"
+
+	router.GET("/hello-world", func(c *gin.Context) {
+
+		c.JSON(200, gin.H{
+
+			"message": "hola Alexander",
+		})
+
+	})
+
+	router.Run() // corremos nuestro servidor sobre el puerto 8080
 
 }
