d2e78b25c615457377d441546137052ab44decf2
AlxndrHernandez <alxndr.hernandez@gmail.com>
2021-12-30 14:53:35 -0300

    se modifica nombre de json y se agrega ejercicio 3

--- main.go
+++ main.go
@@ -1,20 +1,59 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
 
-// Crea router con gin
+	"github.com/gin-gonic/gin"
+)
+
+type Usuarios struct {
+	id            int    `json:"id"`
+	nombre        string `json:"nombre"`
+	apellido      string `json:"apellido"`
+	email         string `json:"email"`
+	edad          int    `json:"edad"`
+	altura        int    `json:"altura"`
+	activo        bool   `json:"activo"`
+	fechaCreacion string `json:"fechaCreacion"`
+}
+
 func main() {
 
+	//-------- ejercicio 2
+	// Crea router con gin
 	router := gin.Default()
 
 	// captura la solicitud get "/hola"
+	router.GET("/saludar : nombre", func(c *gin.Context) {
 
-	router.GET("/hello-world", func(c *gin.Context) {
-
+		nombre := c.Param("nombre")
 		c.JSON(200, gin.H{
 
-			"message": "hola Alexander",
+			"message": "hola " + nombre,
 		})
+
+	})
+
+	//--------- ejercicio 3
+
+	datosJson, err := ioutil.ReadFile("users.json")
+	if err != nil {
+
+		log.Fatal(err)
+	}
+
+	usuarios := Usuarios{}
+	err = json.Unmarshal(datosJson, &usuarios)
+	if err != nil {
+
+		log.Fatal(err)
+	}
+
+	router.GET("/usuarios", func(c *gin.Context) {
+
+		c.JSON(200, usuarios)
 
 	})
 
